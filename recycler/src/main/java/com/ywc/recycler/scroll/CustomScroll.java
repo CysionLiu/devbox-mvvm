7fa3925cfedf9708514562288349e5505f8e276a
CysionLiu <liuxshang@163.com>
2018-08-21 11:21:56 +0800

    recycler版本2

--- recycler/src/main/java/com/ywc/recycler/scroll/CustomScroll.java
+++ recycler/src/main/java/com/ywc/recycler/scroll/CustomScroll.java
@@ -62,6 +62,7 @@ public class CustomScroll extends RecyclerView.OnScrollListener{
     @Override
     public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
         super.onScrollStateChanged(recyclerView, newState);
+        Log.d("H","滚动状态"+newState);
         if (newState==SCROLL_STATE_IDLE&&customAdapter!=null)
             initStop(recyclerView);
     }
